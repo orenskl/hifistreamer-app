9a9eba4351cf30ff38385ba9e96f3ca794e208ec
Oren Sokoler <orenskl@gmail.com>
2023-02-19 12:37:16 +0200

    Added path with version to package

--- package.sh
+++ package.sh
@@ -1,9 +1,8 @@
 VER=$(awk -F "=" '/version/ {print $2}' setup.cfg | tr -d ' ')
-echo hifistreamer-app-${VER}
-tar --exclude hifistreamer-app-${VER}.tar.gz \
-    --exclude __pycache__ \
+tar --exclude __pycache__ \
     --exclude .git \
     --exclude .DS_Store \
     --exclude venv \
     --exclude frontend/node_modules \
-    -czvf hifistreamer-app-${VER}.tar.gz .
+    --transform "s/^./hifistreamer-app-${VER}/" \
+    -czvf /tmp/hifistreamer-app-${VER}.tar.gz .
