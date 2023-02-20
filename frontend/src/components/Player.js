6fc7985105c92096a64177a5c59659d96ca67063
Oren Sokoler <orenskl@gmail.com>
2023-02-20 11:50:39 +0200

    Skip Iris introduction screen

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -7,7 +7,7 @@ function Player(props) {
     return (
         <div className='Page-container'>
           <iframe title='Mopidy-Iris' 
-                  src={getAPIurl() + ':6680/iris'}
+                  src={getAPIurl() + ':6680/iris/?ui={"initial_setup_complete":true}'}
                   className='Page-content'/>
         </div>
     );
