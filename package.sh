9855ead478850ca2e9d891e332df640a68adb4da
Oren Sokoler <orenskl@gmail.com>
2023-03-06 22:03:03 +0200

    Added output directory parameter to package.sh

--- package.sh
+++ package.sh
@@ -1,3 +1,11 @@
+#
+# Check for command line arguments
+#
+if [ "$1" == "" ] || [ $# -gt 1 ]; then
+    echo "Please specify output directory"
+    exit 1
+fi
+
 VER=$(awk -F "=" '/version/ {print $2}' setup.cfg | tr -d ' ')
 if [[ $(tar --version | grep bsdtar) ]]; then
     PATH_ARG="-s /^./hifistreamer-app-${VER}/"
@@ -10,4 +18,4 @@ tar --exclude __pycache__ \
     --exclude venv \
     --exclude frontend/node_modules \
     ${PATH_ARG} \
-    -czvf ~/projects/hifistreamer-app-${VER}.tar.gz .
+    -czvf $1/hifistreamer-app-${VER}.tar.gz .
